25b075efdcda0e00d6114910826db55c4d7f0109
Mark Bradley <braddle@gmail.com>
2020-03-31 22:09:37 +0100

    Setting up project
      - Installing Dependencies
        - Dev PHPUnit and Guzzle
        - Prod Slim Framework
      - Setting up test, web and app directories
      - Setting holding End to End and Unit tests

---
+++ test/e2e/HealthCheckTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Braddle;
+
+use PHPUnit\Framework\TestCase;
+
+class HealthCheckTest extends TestCase
+{
+    public function testBlah()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ test/unit/PlaceHolderTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Braddle;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceHolderTest extends TestCase
+{
+    public function testPlaceholder()
+    {
+        $this->assertTrue(true);
+    }
+}
